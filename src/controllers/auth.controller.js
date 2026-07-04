2c660ea7888923e73472fd8ddca89ca4391ccba2
vishal-gupta5 <guptaaryan1103@gmail.com>
2026-07-04 18:49:19 +0530

    Update the file

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -85,8 +85,8 @@ const login = async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
-    });
-
+    }); 
+  
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "lax",
